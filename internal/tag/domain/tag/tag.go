4fdb1278db55c4fe26629fbc28baefe315b12794
levinhne <levinhne@gmail.com>
2022-04-12 21:08:44 +0700

    update code

--- internal/tag/domain/tag/tag.go
+++ internal/tag/domain/tag/tag.go
@@ -11,6 +11,7 @@ type Tag struct {
 	Id         string    `bson:"_id,omitempty"`
 	Name       string    `bson:"name,omitempty" json:"name,omitempty"`
 	Desciption string    `bson:"description,omitempty" json:"description,omitempty"`
+	Type       string    `bson:"type,omitempty" json:"type,omitempty"`
 	CreatedAt  time.Time `bson:"created_at,omitempty" json:"created_at,omitempty"`
 	UpdatedAt  time.Time `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
 }
@@ -20,9 +21,9 @@ func (t *Tag) ToProtoMessage() *tagpb.Tag {
 		Id:          t.Id,
 		Name:        t.Name,
 		Description: t.Desciption,
-		Type:        "",
+		Type:        t.Type,
 		Status:      tagpb.Tag_ACTIVE,
-		PublishedAt: &timestamppb.Timestamp{Seconds: t.CreatedAt.Unix()},
+		CreatedAt:   &timestamppb.Timestamp{Seconds: t.CreatedAt.Unix()},
 		UpdatedAt:   &timestamppb.Timestamp{Seconds: t.CreatedAt.Unix()},
 	}
 }
